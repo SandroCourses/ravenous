45fb683356cf88fa58700e3939218bebca02ae37
SandroCourses <sandroneumann@me.com>
2018-05-20 15:47:31 +0200

    Fourth version of project

--- src/App/App.js
+++ src/App/App.js
@@ -3,25 +3,24 @@ import logo from '../logo.svg';
 import './App.css';
 import BusinessList from '../components/BusinessList/BusinessList';
 import SearchBar from '../components/SearchBar/SearchBar';
+import Yelp from '../util/Yelp';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'ITALIAN',
-  rating: 4.5,
-  reviewCount: 90
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
 
-const businesses = [business, business, business, business, business, business];
+    this.searchYelp = this.searchYelp.bind(this);
+  }
 
-class App extends Component {
   searchYelp(term, location, sortBy) {
-    let msg = `Searching for ${term} in ${location} with sorting option: ${sortBy}`;
-    console.log(msg);
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({
+        businesses: businesses
+      });
+    });
   }
 
   render() {
@@ -29,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
---
+++ src/util/Yelp.js
@@ -0,0 +1,36 @@
+const apiKey = 'yxUpk_RllvWTOmrXu_ndQOOiOzQV1N3jqVW2181fV6CcC6LDPb1MGTvZPveBwePnn' +
+  'VAiQOZTBbVBB-P7s4VyVYFpHJmnqEyMeWd904_rN4LOSpoffcx-NdV6vmQBW3Yx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      throw new Error('request failed');
+    }, networkError => console.log(networkError.message)).then(jsonResponse => {
+      if(jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          };
+        });
+      }
+    });
+  }
+};
+
+export default Yelp;
